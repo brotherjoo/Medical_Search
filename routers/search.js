0efeb81538c19eacfc7326b224588df9fe9ad2ca
Hyungjoo Kim <khj01173@naver.com>
2024-06-11 21:36:19 +0900

    search.js code remake

--- routers/search.js
+++ routers/search.js
@@ -22,57 +22,57 @@ router.post("/", (req, res, next) => {
 
     let concordance_rate = [];
 
-    for (let i = 0; i < symptoms.length; i++) {
-        for (let j = 0; j < input.length; j++) {
-            for (let k = 0; k < symptoms[i].main_symptom.length; k++) {
-                if (input[j] == symptoms[i].main_symptom[k]) {
-                    let check = false;
-                    for (let l = 0; l < concordance_rate.length; l++) {         //있으면 number++
-                        if (typeof concordance_rate[l] !== undefined & concordance_rate[l].name == symptoms[i].name) {
-                            concordance_rate[l].number++;
-                            check = true;
-                            break;
-                        }
-                    }
-                    if (!check) {           // concordance배열에 질병 이름이 없으면 새로 추가(name, number만 추가됨)
-                        concordance_rate.push({ name: symptoms[i].name, number: 1, url: symptoms[i].url });
-                        // shortenlink(symptoms[i].url).then(x => {
-                        //     concordance_rate[concordance_rate.length].url = x
-                        // }
-                        // ).catch(error => {
-                        //     console.log('Failed to shorten URL:', error);
-                        // }
-                        // );
-                        // // concordance_rate.push({
-                        // //     name: symptoms[i].name, number: 1, url: shortenlink(symptoms[i].url).then().catch()
-                        // // });
-                    }
-                }
-            }
-        }
-    }
-    // for문 4중 반복의 특성때문에 검색 시간이 길어질것을 염려하여
-    // 추후 밑의 코드를 개선해서 삭제 예정
-
-    // const clear_symptom = symptoms.map((x) => {
-    //     const temp = x.main_symptom;
-    //     return temp;
-    // });
-    // for (let i = 0; i < clear_symptom.length; i++) {
-    //     for (let j = 0; j < result.length; j++) {
-    //         if (result[j] == clear_symptom[i]) {
-    //             for (let k = 0; k < concordance_rate.length; k++) {
-    //                 if (symptoms[i].name == concordance_rate[k].name) {
-    //                     concordance_rate[k].number++;
-    //                 } else {
-    //                     concordance_rate.push({ name: symptoms[i].name, number: 1 });
+    // for (let i = 0; i < symptoms.length; i++) {
+    //     for (let j = 0; j < input.length; j++) {
+    //         for (let k = 0; k < symptoms[i].main_symptom.length; k++) {
+    //             if (input[j] == symptoms[i].main_symptom[k]) {
+    //                 let check = false;
+    //                 for (let l = 0; l < concordance_rate.length; l++) {         //있으면 number++
+    //                     if (typeof concordance_rate[l] !== undefined & concordance_rate[l].name == symptoms[i].name) {
+    //                         concordance_rate[l].number++;
+    //                         check = true;
+    //                         break;
+    //                     }
+    //                 }
+    //                 if (!check) {           // concordance배열에 질병 이름이 없으면 새로 추가(name, number만 추가됨)
+    //                     concordance_rate.push({ name: symptoms[i].name, number: 1, url: symptoms[i].url });
+    //                     // shortenlink(symptoms[i].url).then(x => {
+    //                     //     concordance_rate[concordance_rate.length].url = x
+    //                     // }
+    //                     // ).catch(error => {
+    //                     //     console.log('Failed to shorten URL:', error);
+    //                     // }
+    //                     // );
+    //                     // // concordance_rate.push({
+    //                     // //     name: symptoms[i].name, number: 1, url: shortenlink(symptoms[i].url).then().catch()
+    //                     // // });
     //                 }
     //             }
     //         }
     //     }
     // }
-    // 개선의 가능성이 보이는 알고리즘
-    // 후에 개발해서 개선할 예정
+    // for문 4중 반복의 특성때문에 검색 시간이 길어질것을 염려하여
+    // 추후 밑의 코드를 개선해서 삭제 예정
+
+    symptoms.forEach(symptom => {
+        input.forEach(i => {
+            symptom.main_symptom.forEach(sym => {
+                if (i === sym) {
+                    let check = false
+                    for (let j = 0; j < concordance_rate.length; j++) {
+                        if (typeof concordance_rate[j] !== undefined & concordance_rate[j].name == symptom.name) {
+                            concordance_rate[j].number++;
+                            check = true
+                            break;
+                        }
+                    }
+                    if (!check) {
+                        concordance_rate.push({ name: symptom.name, number: 1, url: symptom.url });
+                    }
+                }
+            })
+        })
+    }) // 개선된 코드
 
     concordance_rate.sort((a, b) => {
         return b.number - a.number;
